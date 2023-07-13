a67165d1d554e34ff06034b6947437536fece100
ZakJaeb <zakjaeb@gmail.com>
2023-07-13 19:44:16 -0400

    Added Bootstrap cards.

--- index.php
+++ index.php
@@ -14,30 +14,20 @@
 		
 		<!-- top -->
 		<div class="row">
-			<div class="col-lg-8">
+			<div class="col-lg-4">
 				<h1>View Grocery List</h1>
 				<a href="add.php">Add Item</a>
 			</div>
-			<div class="col-lg-4">
-				<div class="row">
-					<div class="col-lg-8">
-						
-						<!-- Date Filtering-->
-						<form method="post" action="">
-							<input type="date"
-								class="form-control"
-								name="idate">
-						
-							<div class="col-lg-4"
-								method="post">
-								<input type="submit"
-								class="btn btn-danger float-right"
-								name="btn" value="filter">
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
+		<div class="row">
+  			<div class="col-sm-6">
+    			<div class="card">
+      				<div class="card-body">
+        				<h5 class="card-title">Special title treatment</h5>
+        				<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        				<a href="#" class="btn btn-primary">Go somewhere</a>
+      				</div>
+    			</div>
+  			</div>
 		</div>
 	</div>
 </body>
